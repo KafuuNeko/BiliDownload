9b4f0c1ecc20185d0a7b66b984de3832a88b9f8b
hhyufan <107835898+hhyufan@users.noreply.github.com>
2024-04-02 20:27:19 +0800

    本次修改了版本号从1.0到2.0.0，code从1到20000

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "cc.kafuu.bilidownload"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 20000
+        versionName = "2.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         externalNativeBuild {
