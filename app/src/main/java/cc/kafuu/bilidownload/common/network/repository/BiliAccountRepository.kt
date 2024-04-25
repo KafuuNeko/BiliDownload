d9fc0929883650a7d1e6e9b2042b9934ba1d2c36
Kafuu Neko <kafuuneko@gmail.com>
2024-04-25 15:26:54 +0800

    feat: 添加异步获取账户数据接口

--- app/src/main/java/cc/kafuu/bilidownload/common/network/repository/BiliAccountRepository.kt
+++ app/src/main/java/cc/kafuu/bilidownload/common/network/repository/BiliAccountRepository.kt
@@ -1,5 +1,6 @@
 package cc.kafuu.bilidownload.common.network.repository
 
+import cc.kafuu.bilidownload.common.network.IServerCallback
 import cc.kafuu.bilidownload.common.network.NetworkConfig
 import cc.kafuu.bilidownload.common.network.manager.WbiManager
 import cc.kafuu.bilidownload.common.network.model.BiliAccountData
@@ -12,8 +13,26 @@ class BiliAccountRepository(biliApiService: BiliApiService) : BiliRepository(bil
         )
         return biliApiService.getAccountData(
             NetworkConfig.buildFullUrl(
-                "x/space/wbi/acc/info", WbiManager.syncGenerateSignature(params)
+                "/x/space/wbi/acc/info", WbiManager.syncGenerateSignature(params)
             )
         ).execute(onFailure) { it }
+    }
+
+    fun getAccountData(mid: Long, callback: IServerCallback<BiliAccountData>) {
+        val params = linkedMapOf<String, Any>(
+            "mid" to mid
+        )
+        WbiManager.asyncGenerateSignature(params, object : IServerCallback<String> {
+            override fun onSuccess(httpCode: Int, code: Int, message: String, data: String) {
+                biliApiService.getAccountData(
+                    NetworkConfig.buildFullUrl(
+                        "/x/space/wbi/acc/info", data
+                    )
+                ).enqueue(callback) { it }
+            }
+            override fun onFailure(httpCode: Int, code: Int, message: String) {
+                callback.onFailure(httpCode, code, message)
+            }
+        })
     }
 }
