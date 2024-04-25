ddb7644e42441213fe33eceabbc0f35c662d4145
Kafuu Neko <kafuuneko@gmail.com>
2024-04-26 00:17:33 +0800

    refactor: 优化SearchListViewModel代码

--- app/src/main/java/cc/kafuu/bilidownload/viewmodel/fragment/SearchListViewModel.kt
+++ app/src/main/java/cc/kafuu/bilidownload/viewmodel/fragment/SearchListViewModel.kt
@@ -5,63 +5,23 @@ import cc.kafuu.bilidownload.R
 import cc.kafuu.bilidownload.common.network.IServerCallback
 import cc.kafuu.bilidownload.common.network.manager.NetworkManager
 import cc.kafuu.bilidownload.common.network.model.BiliSearchData
+import cc.kafuu.bilidownload.common.network.model.BiliSearchResultData
 import cc.kafuu.bilidownload.common.utils.CommonLibs
 import cc.kafuu.bilidownload.model.BiliVideo
 import cc.kafuu.bilidownload.model.LoadingStatus
 import com.bumptech.glide.load.resource.bitmap.CenterCrop
 
-class SearchListViewModel : RVViewModel() {
+class SearchListViewModel : RVViewModel(), IServerCallback<BiliSearchData> {
     val centerCrop = CenterCrop()
 
     companion object {
         private const val TAG = "SearchListViewModel"
     }
 
-    fun doSearch(keyword: String) {
-        Log.d(TAG, "doSearch: $keyword")
-        if (loadingStatusMessageMutableLiveData.value == LOADING_STATUS_LOADING) {
-            Log.d(TAG, "doSearch: Search execution")
-            return
-        }
-        loadingStatusMessageMutableLiveData.value = LOADING_STATUS_LOADING
-        NetworkManager.biliSearchRepository.search(
-            keyword,
-            object : IServerCallback<BiliSearchData> {
-                override fun onSuccess(
-                    httpCode: Int,
-                    code: Int,
-                    message: String,
-                    data: BiliSearchData
-                ) {
-                    onSearchRespond(data)
-                }
-
-                override fun onFailure(httpCode: Int, code: Int, message: String) {
-                    Log.d(TAG, "onFailure: httpCode($httpCode), code($code), message($message)")
-                    loadingStatusMessageMutableLiveData.postValue(
-                        LoadingStatus(
-                            true,
-                            CommonLibs.getDrawable(R.drawable.ic_error),
-                            message
-                        )
-                    )
-                }
-            })
-    }
-
-    private fun onSearchRespond(data: BiliSearchData) {
-        Log.d(TAG, "onSearchRespond: $data")
+    override fun onSuccess(httpCode: Int, code: Int, message: String, data: BiliSearchData) {
         val searchData: MutableList<Any> = mutableListOf()
         data.result.forEach { biliSearchResult ->
-            val result = biliSearchResult.data.filter { it.type == "video" }.map {
-                BiliVideo(
-                    it.bvid,
-                    it.title,
-                    it.description,
-                    "https:${it.pic}"
-                )
-            }
-            searchData.addAll(result)
+            searchData.addAll(getResultVideos(biliSearchResult.data))
         }
         loadingStatusMessageMutableLiveData.postValue(
             if (searchData.isEmpty()) {
@@ -71,5 +31,32 @@ class SearchListViewModel : RVViewModel() {
             }
         )
         listMutableLiveData.postValue(searchData)
+    }
+
+    override fun onFailure(httpCode: Int, code: Int, message: String) {
+        Log.d(TAG, "onFailure: httpCode($httpCode), code($code), message($message)")
+        loadingStatusMessageMutableLiveData.postValue(
+            LoadingStatus(
+                true,
+                CommonLibs.getDrawable(R.drawable.ic_error),
+                message
+            )
+        )
+    }
+
+    fun doSearch(keyword: String) {
+        Log.d(TAG, "doSearch: $keyword")
+        if (loadingStatusMessageMutableLiveData.value == LOADING_STATUS_LOADING) {
+            Log.d(TAG, "doSearch: Search execution")
+            return
+        }
+        loadingStatusMessageMutableLiveData.value = LOADING_STATUS_LOADING
+        NetworkManager.biliSearchRepository.search(keyword, this)
+    }
+
+    private fun getResultVideos(biliSearchResultDataList: List<BiliSearchResultData>): List<BiliVideo> {
+        return biliSearchResultDataList.filter { it.type == "video" }.map {
+            BiliVideo(it.bvid, it.title, it.description, "https:${it.pic}")
+        }
     }
 }
