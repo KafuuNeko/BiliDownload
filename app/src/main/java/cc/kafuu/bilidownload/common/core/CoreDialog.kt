098c1d89b95000f6738b9d65b6b143928aab78d1
Kafuu Neko <kafuuneko@gmail.com>
2024-05-08 18:11:02 +0800

    feat: 实现CoreDialog

---
+++ app/src/main/java/cc/kafuu/bilidownload/common/core/CoreDialog.kt
@@ -0,0 +1,39 @@
+package cc.kafuu.bilidownload.common.core
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.DialogFragment
+import cc.kafuu.bilidownload.R
+
+
+abstract class CoreDialog<V : ViewDataBinding>(
+    @LayoutRes private val mLayoutId: Int
+) : DialogFragment() {
+    protected lateinit var mViewDataBinding: V
+    protected abstract fun initViews()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setStyle(STYLE_NO_TITLE, R.style.Theme_Dialog)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        mViewDataBinding = DataBindingUtil.inflate(inflater, mLayoutId, container, false)
+        return mViewDataBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initViews()
+    }
+}
+
