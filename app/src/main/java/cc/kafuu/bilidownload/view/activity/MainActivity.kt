9b24cf72c82d0455600abcc50909095efdba8e40
Kafuu Neko <kafuuneko@gmail.com>
2024-05-09 18:34:10 +0800

    feat: 删MainActivity测试代码

--- app/src/main/java/cc/kafuu/bilidownload/view/activity/MainActivity.kt
+++ app/src/main/java/cc/kafuu/bilidownload/view/activity/MainActivity.kt
@@ -29,68 +29,12 @@ class MainActivity : CoreActivity<ActivityMainBinding, MainViewModel>(
     R.layout.activity_main,
     BR.viewModel
 ) {
-
     companion object {
-        private const val TAG = "MainActivity"
         private val mScope = CoroutineScope(Dispatchers.Default + SupervisorJob())
-    }
-
-    var first: Boolean = true
-
-    fun downloadTest() {
-        NetworkManager.biliVideoRepository.getPlayStreamData(
-            "BV1Ai4y197Wt",
-            481763422,
-            object :
-                IServerCallback<BiliPlayStreamData> {
-                override fun onSuccess(
-                    httpCode: Int,
-                    code: Int,
-                    message: String,
-                    data: BiliPlayStreamData
-                ) {
-                    Log.d(
-                        TAG,
-                        "onSuccess: httpCode: $httpCode, code: $code, message: $message, data: $data"
-                    )
-
-                    Log.d(
-                        TAG,
-                        "onSuccess: ${data.dash.video.map { "${it.id}, ${it.codecs}, ${it.mimeType}" }}}"
-                    )
-                    runBlocking {
-                        val taskId = CommonLibs.requireAppDatabase().downloadTaskDao().insert(
-                            DownloadTaskEntity.createEntity(
-                                "BV1Ai4y197Wt",
-                                481763422,
-                                data.dash.video[2],
-                                data.dash.audio[2]
-                            )
-                        )
-                        DownloadService.startDownload(this@MainActivity, taskId)
-                        Log.d(TAG, "onSuccess: $taskId")
-                    }
-
-                }
-
-                override fun onFailure(httpCode: Int, code: Int, message: String) {
-                    Log.e(TAG, "onFailure: httpCode: $httpCode, code: $code, message: $message")
-                }
-
-            })
-//        FFMpegJNI.mergeMedia(
-//            "/storage/emulated/0/Android/data/cc.kafuu.bilidownload/files/cache/download/task-e10/merge2.mp4",
-//            arrayOf(
-//                "/storage/emulated/0/Android/data/cc.kafuu.bilidownload/files/cache/download/task-e10/1169319048-1-30216.mp4",
-//                "/storage/emulated/0/Android/data/cc.kafuu.bilidownload/files/cache/download/task-e10/1169319048-1-30032.mp4"
-//            ),
-//            arrayOf()
-//        )
     }
 
     override fun initViews() {
         setImmersionStatusBar()
-        //downloadTest()
         mViewDataBinding.vp2Content.apply {
             adapter = FragmentAdapter(supportFragmentManager, lifecycle).apply {
                 addFragmentView(getFragments())
@@ -104,16 +48,6 @@ class MainActivity : CoreActivity<ActivityMainBinding, MainViewModel>(
         mViewModel.tabPositionLiveData.observe(this) { position ->
             if (mViewDataBinding.vp2Content.currentItem !== position) {
                 mViewDataBinding.vp2Content.setCurrentItem(position, false)
-            } else {
-                if (first) {
-                    first = false
-                } else {
-                    if (!PermissionUtils.hasStoragePermissions(this)) {
-                        PermissionUtils.requestStoragePermissions(this)
-                    } else {
-                        downloadTest()
-                    }
-                }
             }
         }
         mScope.launch { DownloadService.resumeDownload(this@MainActivity) }
