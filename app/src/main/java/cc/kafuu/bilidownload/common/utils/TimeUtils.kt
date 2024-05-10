e6d7d68e4a76c920a45bb67764ee68dae8c57ca1
Kafuu Neko <kafuuneko@gmail.com>
2024-05-10 13:31:46 +0800

    feat: 优化视频页面UI

---
+++ app/src/main/java/cc/kafuu/bilidownload/common/utils/TimeUtils.kt
@@ -0,0 +1,11 @@
+package cc.kafuu.bilidownload.common.utils
+
+object TimeUtils {
+    fun formatSecondTime(seconds: Int): String {
+        val hours = seconds / 3600
+        val minutes = (seconds % 3600) / 60
+        val secs = seconds % 60
+
+        return String.format("%02d:%02d:%02d", hours, minutes, secs)
+    }
+}
