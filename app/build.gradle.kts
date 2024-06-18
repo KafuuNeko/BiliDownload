cb8d5c30c714af1ad3084f372824e27a189d456e
Kafuu Neko <kafuuneko@gmail.com>
2024-06-18 22:20:12 +0800

    版本迭代：2.1.2.foss

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "cc.kafuu.bilidownload"
         minSdk = 24
         targetSdk = 34
-        versionCode = 2_01_01
-        versionName = "2.1.1.foss"
+        versionCode = 2_01_02
+        versionName = "2.1.2.foss"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
