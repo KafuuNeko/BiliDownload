d5b82d4b46f7b8ccd2cef919394ece5dd14b87d9
KafuuNeko <kafuuneko@gmail.com>
2026-05-09 13:53:33 +0800

    版本迭代：2.3.2
    1. #7 #49 支持用户修改下载路径
    2. 播放页支持倍率播放以及其他应用打开
    3. 替换内置下载引擎，解决断点续传问题
    4. 个人详情页添加搜索功能

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "cc.kafuu.bilidownload"
         minSdk = 24
         targetSdk = 35
-        versionCode = 2_03_01
-        versionName = "2.3.1.foss"
+        versionCode = 2_03_02
+        versionName = "2.3.2.foss"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
